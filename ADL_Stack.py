d9d8813e4f1c55f6e786483466e85043305f1fad
Adland Lee <adland.lee@gmail.com>
2019-05-23 01:33:20 -0700

    implement stack

---
+++ ADL_Stack.py
@@ -0,0 +1,53 @@
+from abc import ABCMeta, abstractmethod
+
+from ADL_SinglyLinkedList import *
+from ADL_DoublyLinkedList import *
+
+class ADL_Stack:
+
+    @property
+    def isEmpty(self):
+        raise NotImplementedError
+    
+    @property
+    def count(self):
+        raise NotImplementedError
+
+    @property
+    def peek(self):
+        raise NotImplementedError
+
+    def push(self, value):
+        raise NotImplementedError
+
+    def pop(self):
+        raise NotImplementedError
+
+
+class ADL_SinglyLinkedList_Stack(ADL_SinglyLinkedList, ADL_Stack):
+    @property
+    def peek(self):
+        return self.head
+
+    def push(self, value):
+        self.insert(value, atIndex=0)
+
+    def pop(self):
+        if self.isEmpty:
+            return None
+        return self.remove(atIndex=0)
+
+
+class ADL_DoublyLinkedList_Stack(ADL_DoublyLinkedList, ADL_Stack):
+    @property
+    def peek(self):
+        return self.last
+
+    def push(self, value):
+        self.append(value)
+
+    def pop(self):
+        if self.isEmpty:
+            return None
+        return self.removeLast()
+
---
+++ ADL_Stack_tests.py
@@ -0,0 +1,58 @@
+import unittest
+from ADL_Stack import *
+
+def test_base(base_cls):
+    class BaseClassSkipper(base_cls):
+        @classmethod
+        def setUpClass(cls):
+            if cls is BaseClassSkipper:
+                raise unittest.SkipTest("Base class")
+            super().setUpClass()
+    return BaseClassSkipper
+
+@test_base
+class Base_ADL_Stack_tests(unittest.TestCase):
+
+    def setUp(self):
+        raise NotImplementedError
+
+    def test_stack_initialization(self):
+        self.assertTrue(self.stack.isEmpty)
+        self.assertEqual(self.stack.count, 0)
+        self.assertIsNone(self.stack.peek)
+
+
+    def test_stack_push(self):
+        for i in range(3):
+            self.stack.push(i)
+            self.assertFalse(self.stack.isEmpty)
+            self.assertEqual(self.stack.peek, i)
+            self.assertEqual(self.stack.count, i+1)
+
+
+    def test_stack_pop(self):
+        self.assertIsNone(self.stack.pop())
+        for i in range(3):
+            self.stack.push(i)
+            self.assertEqual(self.stack.peek, i)
+
+        for i in range(2, 0-1, -1):
+            self.assertEqual(self.stack.peek, i)
+            self.assertEqual(self.stack.pop(), i)
+            self.assertEqual(self.stack.count, i)
+
+
+class test_ADL_SinglyLinkedList_Stack(Base_ADL_Stack_tests):
+
+    def setUp(self):
+        self.stack = ADL_SinglyLinkedList_Stack()
+
+
+class test_ADL_DoublyLinkedList_Stack(Base_ADL_Stack_tests):
+
+    def setUp(self):
+        self.stack = ADL_DoublyLinkedList_Stack()
+
+
+if __name__ == '__main__':
+    unittest.main()
