906845f10f5854da19ab266d0cc3cf559964989a
Adland Lee <adland.lee@gmail.com>
2019-07-20 16:34:18 -0700

    refactor singly linked list

--- ADL_SinglyLinkedList.py
+++ ADL_SinglyLinkedList.py
@@ -1,179 +1,39 @@
+from os import abort
+
 class ADL_SinglyLinkedList:
     """Singly Linked List Implementation"""
 
-    class Node:
-
-        def __init__(self):
-            self.next = None
-            self.value = None
-
-
-        @property
-        def value(self):
-            return self._value
-        @value.setter
-        def value(self, newValue):
-            self._value = newValue
-
-
-        @property
-        def next(self):
-            return self._next
-        @next.setter
-        def next(self, newValue):
-            self._next = newValue
+    def __init__(self, value):
+        self._next = None
+        self._value = value
 
 
     @property
-    def startNode(self):
-        return getattr(self, "_startNode", None)
-    @startNode.setter
-    def startNode(self, newValue):
-        self._startNode = newValue
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, newValue):
+        self._value = newValue
 
 
     @property
-    def endNode(self):
-        return getattr(self, "_endNode", None)
-    @endNode.setter
-    def endNode(self, newValue):
-        self._endNode = newValue
+    def next(self):
+        return self._next
+    @next.setter
+    def next(self, newValue):
+        self._next = newValue
 
-
-    @property
-    def count(self):
-        return len(self)
 
     def __len__(self):
-        results = 0
-        node = self.startNode
-        while node is not None:
-            results += 1
-            node = node.next
-        return results
+        """Return the number of nodes from this node"""
+        if self.next is None:
+            return 1
 
+        return 1 + self.next.__len__()
 
-    @property
-    def isEmpty(self):
-        return self.startNode is None
-    
-
-    @property
-    def head(self):
-        return getattr(self.startNode, "value", None)
-
-
-    @property
-    def tail(self):
-        if self.count < 2:
-            return None
-
-        newList = ADL_SinglyLinkedList()
-        newList.head = getattr(self.startNode, "next", None)
-        return newList
-        
-
-    def insert(self, value, atIndex):
-        assert 0 <= atIndex and atIndex <= self.count, "index out of bounds"
-
-        index = atIndex
-
-        newNode = self.Node()
-        newNode.value = value
-
-        if index == 0:
-            if self.isEmpty:
-                self.endNode = newNode
-
-            newNode.next = self.startNode
-
-            self.startNode = newNode
-        else:
-            nodeAtIndex = self.startNode
-            for i in range(1, index):
-                nodeAtIndex = nodeAtIndex.next
-
-            if index == self.count:
-                self.endNode = newNode
-
-            newNode.next = nodeAtIndex.next
-            nodeAtIndex.next = newNode
-
-
-
-    def getValue(self, atIndex):
-        assert 0 <= atIndex and atIndex < self.count, "index out of bounds"
-        index = atIndex
-
-        nodeAtIndex = self.startNode
-        for i in range(index):
-            nodeAtIndex = nodeAtIndex.next
-
-        return nodeAtIndex.value
-
-
-    def append(self, value):
-        self.insert(value, len(self))
-
-
-    def update(self, value, atIndex):
-        assert 0 <= atIndex and atIndex <= self.count, "index out of bounds"
-        index = atIndex
-
-        nodeAtIndex = self.startNode
-        for i in range(index):
-            nodeAtIndex = nodeAtIndex.next
-
-        nodeAtIndex.value = value
-
-    def remove(self, atIndex):
-        assert 0 <= atIndex and atIndex < self.count, "index out of bounds"
-        index = atIndex
-
-        node = self.startNode
-
-        if index == 0:
-            self.startNode = getattr(node, "next", None)
-        else:
-            precedingNode = self.startNode
-            for i in range(1, index):
-                precedingNode = precedingNode.next
-
-            node = precedingNode.next
-            precedingNode.next = node.next
-
-            if index == self.count:
-                self.endNode = precedingNode
-
-        return node.value
-
-    
-    def __getitem__(self, index):
-        return self.getValue(index)
-
-    def __setitem__(self, key, value):
-        self.update(value, key)
-
-
-    class Iterator:
-        def __init__(self, node):
-            self.node = node
-
-        def __iter__(self):
-            return self
-
-        def __next__(self):
-            if self.node is None:
-                raise StopIteration
-
-            value = self.node.value
-            self.node = self.node.next
-            return value
-
-    def __iter__(self):
-        return self.Iterator(self.startNode)
 
     def __str__(self):
+        """Return the string description of this list"""
         s = "["
         sep = ""
         for i in self:
@@ -182,6 +42,129 @@ class ADL_SinglyLinkedList:
         s += "]"
         return s
 
+
+    def __getitem__(self, index):
+        assert 0 <= index and index < len(self), "index out of bounds"
+
+        nodeAtIndex = self
+        for i in range(index):
+            nodeAtIndex = nodeAtIndex.next
+
+        return nodeAtIndex
+
+    def __setitem__(self, key, value):
+        assert 0 <= index and index < len(self), "index out of bounds"
+
+        nodeAtIndex = self
+        for i in range(index):
+            nodeAtIndex = nodeAtIndex.next
+
+        oldValue = nodeAtIndex.value
+        nodeAtIndex.value = value
+        return oldValue
+
+
+    class Iterator:
+        def __init__(self, node):
+            self._node = node
+
+        def __next__(self):
+            if self._node is None:
+                raise StopIteration
+
+            value = self._node.value
+            self._node = self._node.next
+            return value
+
+
+    def __iter__(self):
+        """Return an iterator"""
+        return self.Iterator(self)
+
+
+    @staticmethod
+    def len(aList):
+        if aList is None:
+            return 0
+        return aList.__len__()
+
+
+    @staticmethod
+    def isEmpty(aList):
+        return aList is None
+    
+
+    @staticmethod
+    def insertNode(aList, newNode, atIndex):
+        count = ADL_SinglyLinkedList.len(aList)
+        assert 0 <= atIndex and atIndex <= count, "index out of bounds"
+        index = atIndex
+
+        if index == 0:
+            newNode.next = aList
+            aList = newNode
+        else:
+            nodeAtIndex = aList
+            for i in range(1, index):
+                nodeAtIndex = nodeAtIndex.next
+            newNode.next = nodeAtIndex.next
+            nodeAtIndex.next = newNode
+
+        return aList
+
+
+    @staticmethod
+    def appendNode(aList, newNode):
+        count = ADL_SinglyLinkedList.len(aList) or 0
+        return ADL_SinglyLinkedList.insertNode(aList, newNode, count)
+
+    @staticmethod
+    def getNode(aList, atIndex):
+        if aList is None:
+            raise AssertionError("Cannot get item from an empty Collection")
+        assert 0 <= atIndex and atIndex < len(self), "index out of bounds"
+        index = atIndex
+
+        nodeAtIndex = self
+        for i in range(index):
+            nodeAtIndex = nodeAtIndex.next
+
+        return nodeAtIndex
+
+
+    @staticmethod
+    def removeNode(aList, atIndex):
+        count = ADL_SinglyLinkedList.len(aList) or 0
+        assert 0 <= atIndex and atIndex < count, "index out of bounds"
+        index = atIndex
+
+        node = aList
+
+        if index == 0:
+            aList = getattr(node, "next", None)
+        else:
+            precedingNode = aList
+            for i in range(1, index):
+                precedingNode = precedingNode.next
+
+            node = precedingNode.next
+            precedingNode.next = node.next
+
+            # if index == self.count:
+            #     self.endNode = precedingNode
+
+        return (aList, node)
+
+
+#     @property
+#     def tail(self):
+#         if self.count < 2:
+#             return None
+
+#         newList = ADL_SinglyLinkedList()
+#         newList.head = getattr(self.startNode, "next", None)
+#         return newList
+        
 
     def __eq__(self, other):
         if isinstance(other, ADL_SinglyLinkedList) \
--- test_ADL_SinglyLinkedList.py
+++ test_ADL_SinglyLinkedList.py
@@ -4,125 +4,172 @@ from ADL_SinglyLinkedList import *
 class Test_SinglyLinkedList(unittest.TestCase):
 
     def setUp(self):
-        self.list = ADL_SinglyLinkedList()
+        self.list = None
+
+    def teardown(self):
+        self.list = None
 
     def test_SinglyLinkedListInitialize(self):
-        self.assertEqual(self.list.count, 0)
-        self.assertTrue(self.list.isEmpty)
-        self.assertIsNone(self.list.head)
-        self.assertIsNone(self.list.tail)
+        self.assertEqual(ADL_SinglyLinkedList.len(self.list), 0)
+        self.assertTrue(ADL_SinglyLinkedList.isEmpty(self.list))
 
-    def test_DoublyLinkedListGetValueOutOfBounds(self):
+    def test_SinglyLinkedListString(self):
+        node0 = ADL_SinglyLinkedList(0)
+        node1 = ADL_SinglyLinkedList(1)
+        node2 = ADL_SinglyLinkedList(2)
+        node3 = ADL_SinglyLinkedList(3)
+        node4 = ADL_SinglyLinkedList(4)
+        node0.next = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+
+        self.assertEqual(str(node0), '[0, 1, 2, 3, 4]')
+
+    def test_SinglyLinkedListGetValueOutOfBounds(self):
         with self.assertRaises(AssertionError):
-            self.list.getValue(atIndex=0)
+            ADL_SinglyLinkedList.getNode(self.list, atIndex=0)
         with self.assertRaises(AssertionError):
-            self.list.getValue(atIndex=1)
-        with self.assertRaises(AssertionError):
-            self.list[0]
-        with self.assertRaises(AssertionError):
-            self.list[1]
+            ADL_SinglyLinkedList.getNode(self.list, atIndex=1)
 
     def test_SinglyLinkedListInsertOutOfBounds(self):
+        newNode = ADL_SinglyLinkedList(1)
         with self.assertRaises(AssertionError):
-            self.list.insert(1, atIndex=1)
+            ADL_SinglyLinkedList.insertNode(self.list, newNode, atIndex=1)
 
     def test_SinglyLinkedListInsert(self):
-        self.list.insert(2, atIndex=0)
-        self.assertEqual(self.list.count, 1)
-        self.assertFalse(self.list.isEmpty)
-        self.assertEqual(self.list[0], 2)
+        node0 = ADL_SinglyLinkedList(0)
+        node1 = ADL_SinglyLinkedList(1)
+        node2 = ADL_SinglyLinkedList(2)
+        node3 = ADL_SinglyLinkedList(3)
+        node4 = ADL_SinglyLinkedList(4)
+
+        self.list = ADL_SinglyLinkedList.insertNode(self.list, node2, atIndex=0)
+        self.assertEqual(len(self.list), 1)
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(self.list[0].value, 2)
         self.assertTrue(self.list == [2])
         self.assertEqual(self.list, [2])
 
-        self.list.insert(0, atIndex=0)
-        self.assertEqual(self.list.count, 2)
-        self.assertFalse(self.list.isEmpty)
-        self.assertEqual(self.list[0], 0)
+        self.list = ADL_SinglyLinkedList.insertNode(self.list, node0, atIndex=0)
+        self.assertEqual(len(self.list), 2)
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(self.list[0].value, 0)
         self.assertTrue(self.list == [0, 2])
+        self.assertEqual(self.list, [0, 2])
 
-        listCount = self.list.count
-        self.list.insert(4, atIndex=listCount)
-        self.assertEqual(self.list.count, 3)
-        self.assertFalse(self.list.isEmpty)
-        self.assertEqual(self.list[listCount], 4)
+        listLen = len(self.list)
+        self.list = ADL_SinglyLinkedList.insertNode(self.list, node4, atIndex=listLen)
+        self.assertEqual(len(self.list), 3)        
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(self.list[listLen].value, 4)
         self.assertTrue(self.list == [0, 2, 4])
+        self.assertEqual(self.list, [0, 2, 4])
 
-        self.list.insert(1, atIndex=1)
-        self.assertEqual(self.list.count, 4)
-        self.assertFalse(self.list.isEmpty)
-        self.assertEqual(self.list[1], 1)
+        self.list = ADL_SinglyLinkedList.insertNode(self.list, node1, atIndex=1)
+        self.assertEqual(len(self.list), 4)
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(self.list[1].value, 1)
         self.assertTrue(self.list == [0, 1, 2, 4])
+        self.assertEqual(self.list, [0, 1, 2, 4])
 
-        self.list.insert(3, atIndex=3)
-        self.assertEqual(self.list.count, 5)
-        self.assertFalse(self.list.isEmpty)
-        self.assertEqual(self.list[3], 3)
+        self.list = ADL_SinglyLinkedList.insertNode(self.list, node3, atIndex=3)
+        self.assertEqual(len(self.list), 5)
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(self.list[3].value, 3)
         self.assertTrue(self.list == [0, 1, 2, 3, 4])
+        self.assertEqual(self.list, [0, 1, 2, 3, 4])
 
     def test_SinglyLinkedListAppend(self):
         start = 0
         end = 4
 
         for i in range(start, end+1):
-            self.list.append(i)
-            self.assertEqual(self.list[self.list.count-1], i)
-            self.assertEqual(self.list.head, start)
+            node = ADL_SinglyLinkedList(i)
+            self.list = ADL_SinglyLinkedList.appendNode(self.list, node)
+            self.assertEqual(self.list[len(self.list)-1].value, i)
+            self.assertEqual(self.list[0].value, start)
 
     def test_SinglyLinkedListUpdate(self):
         start = 0
         end = 4
 
         for i in range(start, end+1):
-            self.list.append(0)
+            node = ADL_SinglyLinkedList(i)
+            self.list = ADL_SinglyLinkedList.appendNode(self.list, node)
 
-        for i in range(start, end+1):
-            self.list[i] = i
-            self.assertEqual(self.list[i], i)
-            self.assertEqual(self.list.head, start)
+        self.list[0].value = 10
+        self.assertEqual(len(self.list), 5)
+        self.assertEqual(self.list, [10, 1, 2, 3, 4])
+        self.assertEqual(self.list.value, 10)
+        self.assertEqual(self.list[0].value, 10)
+
+        self.list[2].value = 12
+        self.assertEqual(len(self.list), 5)
+        self.assertEqual(self.list, [10, 1, 12, 3, 4])
+        self.assertEqual(self.list.value, 10)
+        self.assertEqual(self.list[2].value, 12)
+
+        self.list[4].value = 14
+        self.assertEqual(len(self.list), 5)
+        self.assertEqual(self.list, [10, 1, 12, 3, 14])
+        self.assertEqual(self.list.value, 10)
+        self.assertEqual(self.list[4].value, 14)
 
     def test_SinglyLinkedListRemoveOutOfBounds(self):
         with self.assertRaises(AssertionError):
-            self.list.remove(atIndex=1)
+            (self.list, node) = ADL_SinglyLinkedList.removeNode(self.list, atIndex=1)
 
     def test_SinglyLinkedListRemove(self):
         for i in range(0, 4+1):
-            self.list.append(i)
+            node = ADL_SinglyLinkedList(i)
+            self.list = ADL_SinglyLinkedList.appendNode(self.list, node)
 
         self.assertTrue(self.list == [0,1,2,3,4])
 
-        self.assertEqual(self.list.remove(atIndex=3), 3)
+        (self.list, node) = ADL_SinglyLinkedList.removeNode(self.list, atIndex=3)
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(node.value, 3)
         self.assertTrue(self.list == [0,1,2,4])
-        self.assertEqual(self.list.count, 4)
-        self.assertEqual(self.list.head, 0)
-        self.assertEqual(self.list[self.list.count-1], 4)
+        self.assertEqual(len(self.list), 4)
+        self.assertEqual(self.list.value, 0)
+        self.assertEqual(self.list[len(self.list)-1].value, 4)
 
-        self.assertEqual(self.list.remove(atIndex=1), 1)
+        (self.list, node) = ADL_SinglyLinkedList.removeNode(self.list, atIndex=1)
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(node.value, 1)
         self.assertTrue(self.list == [0,2,4])
-        self.assertEqual(self.list.count, 3)
-        self.assertEqual(self.list.head, 0)
-        self.assertEqual(self.list[self.list.count-1], 4)
+        self.assertEqual(len(self.list), 3)
+        self.assertEqual(self.list.value, 0)
+        self.assertEqual(self.list[len(self.list)-1].value, 4)
 
-        self.assertEqual(self.list.remove(atIndex=self.list.count-1), 4)
-        self.assertEqual(self.list.count, 2)
+        (self.list, node) = ADL_SinglyLinkedList.removeNode(self.list, atIndex=len(self.list)-1)
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(node.value, 4)
         self.assertTrue(self.list == [0,2])
-        self.assertEqual(self.list.head, 0)
-        self.assertEqual(self.list[self.list.count-1], 2)
+        self.assertEqual(len(self.list), 2)
+        self.assertEqual(self.list.value, 0)
+        self.assertEqual(self.list[len(self.list)-1].value, 2)
 
-        self.assertEqual(self.list.remove(atIndex=0), 0)
+        (self.list, node) = ADL_SinglyLinkedList.removeNode(self.list, atIndex=0)
+        self.assertFalse(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(node.value, 0)
         self.assertTrue(self.list == [2])
-        self.assertEqual(self.list.count, 1)
-        self.assertEqual(self.list.head, 2)
-        self.assertEqual(self.list[self.list.count-1], 2)
+        self.assertEqual(len(self.list), 1)
+        self.assertEqual(self.list.value, 2)
+        self.assertEqual(self.list[len(self.list)-1].value, 2)
 
-        self.assertEqual(self.list.remove(atIndex=0), 2)
-        self.assertTrue(self.list == [])
-        self.assertEqual(self.list.count, 0)
-        self.assertIsNone(self.list.head)
+        (self.list, node) = ADL_SinglyLinkedList.removeNode(self.list, atIndex=0)
+        self.assertTrue(ADL_SinglyLinkedList.isEmpty(self.list))
+        self.assertEqual(node.value, 2)
+        self.assertTrue(self.list == None)
+        self.assertEqual(ADL_SinglyLinkedList.len(self.list), 0)
 
 
     def test_SinglyLinkedListIterator(self):
         for i in range(5):
-            self.list.append(i)
+            node = ADL_SinglyLinkedList(i)
+            self.list = ADL_SinglyLinkedList.appendNode(self.list, node)
 
         for k,v in enumerate(self.list):
             self.assertEqual(k, v)
