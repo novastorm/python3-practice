b62c0ec3dffaad7abfa1c49d2a46e31831785117
Adland Lee <adland.lee@gmail.com>
2019-09-15 02:38:32 -0700

    update swift enum

--- swiftEnum/src/swiftEnum/__init__.py
+++ swiftEnum/src/swiftEnum/__init__.py
@@ -3,6 +3,8 @@ import enum
 class SwiftEnum(tuple, enum.Enum):
     def __new__(cls, clsargs):
         value = len(cls.__members__) + 1
+
+
         def func(*args):
             if len(args) != len(clsargs):
                 raise TypeError('{}.{}() takes {} arguments ({} given)'.format(
@@ -14,15 +16,26 @@ class SwiftEnum(tuple, enum.Enum):
             obj._value_ = value
             obj._name_ = func._name_
             return obj
+
+
         func._value_ = func.value = value
+
         return func
+    
+
     def __str__(self):
         return '{}.{}({})'.format(type(self).__name__,
                                   self.name,
                                   ', '.join(map(repr, self)))
+    
+
     __repr__ = __str__
+
+    
     def __hash__(self, other):
         return hash((self._name_, tuple.__hash__(self)))
+
+
     def __eq__(self, other):
         return (tuple.__eq__(self, other) and
                 type(self) == type(other) and
