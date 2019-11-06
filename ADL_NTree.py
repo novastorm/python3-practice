36d08fead573533fc84d603e20efc1511f932001
Adland Lee <adland.lee@gmail.com>
2019-11-06 00:32:16 -0800

    update N-Tree

--- ADL_NTree.py
+++ ADL_NTree.py
@@ -6,17 +6,21 @@ class NTreeError(Exception):
     pass
 
 
-class NodeTypeError(NTreeError):
+class NTreeNodeTypeError(NTreeError):
     """Node was not an instance of :class:`binarytree.Node`."""
     pass
 
 
-class NodeValueError(NTreeError):
+class NTreeNodeValueError(NTreeError):
     """Node value was not a valid."""
     pass
 
 
 class ADL_NTreeNode:
+
+    def __init__(self, value):
+        self._value = value
+        self._children = []
 
     @property
     def value(self):
@@ -34,8 +38,8 @@ class ADL_NTreeNode:
     def children(self, newValue):
         self._children = newValue
 
-    def __len__(self):
-        return 1 + (sum([len(c) for c in self.children]) if self.children else 0)
+    # def __len__(self):
+    #     return 1 + (sum([len(c) for c in self.children]) if self.children else 0)
 
 
 class ADL_NTree:
@@ -98,7 +102,7 @@ class ADL_NTreeNode_Iterative(ADL_NTreeNode, ADL_NTree):
         if children:
             for node in children:
                 if node is not None and not isinstance(node, ADL_NTree):
-                   raise NodeTypeError('all children nodes   must be a ADL_NTree instance')
+                   raise NTreeNodeTypeError('all children nodes   must be a ADL_NTree instance')
 
         self._value = value
         self._children = children
@@ -127,6 +131,25 @@ class ADL_NTreeNode_Iterative(ADL_NTreeNode, ADL_NTree):
     def breadthFirst(self):
         return self.BreadthFirstIterator(self)
 
+
+    class PreOrderIterator:
+        def __init__(self, node):
+            self.stack = []
+            self.stack.append(node)
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if not self.stack:
+                raise StopIteration
+
+            next = self.stack.pop()
+
+            for n in next.children[::-1]:
+                self.stack.append(n)
+
+            return next.value
 
     class PostOrderIterator:
 
@@ -169,3 +192,38 @@ class ADL_NTreeNode_Iterative(ADL_NTreeNode, ADL_NTree):
     def postOrder(self):
         return self.PostOrderIterator(self)
 
+
+###############################################################################
+
+import unittest
+
+
+class Test_NTree(unittest.TestCase):
+
+    def test_1(self):
+
+        nodes = {}
+
+        for i in range(16):
+            nodes[i] = ADL_NTreeNode(i)
+
+        root = nodes[0]
+        nodes[0].children = [nodes[1], nodes[2], nodes[3]]
+        nodes[1].children = [nodes[4], nodes[5], nodes[6]]
+        nodes[2].children = [nodes[7], nodes[8], nodes[9]]
+        nodes[3].children = [nodes[10], nodes[11], nodes[12]]
+        nodes[4].children = [nodes[13], nodes[14], nodes[15]]
+
+        aBFS = ADL_NTreeNode_Iterative.BreadthFirstIterator(root)
+        print(list(aBFS))
+        aPreDFS = ADL_NTreeNode_Iterative.PreOrderIterator(root)
+        print(list(aPreDFS))
+        aPostDFS = ADL_NTreeNode_Iterative.PostOrderIterator(root)
+        print(list(aPostDFS))
+
+        i = None
+        e = None
+        self.assertEqual(i, e)
+
+if __name__ == '__main__':
+    unittest.main()
